a435c7c3f43855b25a74f2687f77041f95c45a40
Phạm Quân <phamquan29hy@gmail.com>
2023-12-09 14:24:57 +0700

    add

---
+++ CreateWebsiteWithLogin/script.js
@@ -0,0 +1,36 @@
+const formOpenBtn= document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer =document.querySelector(".form_container"),
+formCloseBtn =document.querySelector(".form_close"),
+signupBtn =document.querySelector("#singup"),
+loginBtn =document.querySelector("#login"),
+pwShowHie =document.querySelectorAll(".pw_hide");
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHie.forEach(icon => {
+    icon.addEventListener("click", () =>{
+        let getPwInput = icon.parentElement.querySelector("input");
+        if(getPwInput.type === "password"){
+            getPwInput.type="text";
+            icon.classList.replace("fa-eye-slash","fa-eye");
+        }else{
+            getPwInput.type="password";
+            icon.classList.replace("fa-eye","fa-eye-slash");
+
+        }
+    });
+});
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
--- Create_A_Draggable_Card_Slider/script.js
+++ Create_A_Draggable_Card_Slider/script.js
@@ -1,9 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
 const carousel =document.querySelector(".carousel");
 const arrowBtns =document.querySelectorAll(".wrapper i");
 const firstCarWith = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
 
 let isDragging=false , isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
+//Nhận số lượng thẻ có thể vừa với băng chuyền cùng một lúc
+let cardPerView=Math.round(carousel.offsetWidth / firstCarWith);
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin",card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
 
 arrowBtns.forEach(btn =>{
     btn.addEventListener("click", () =>{
@@ -29,6 +42,31 @@ const dragStop = () =>{
     carousel.classList.remove("dragging");
 }
 
+
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft == 0){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.add("no-transition");
+    }
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+
+}
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
